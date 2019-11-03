afa4a3fc0acc7249f46007751612af5bfaa54770
Darren Lee <darren.lee@nexybit.com>
2019-11-03 22:18:00 +0900

    todo component refactoring

--- src/store/modules/todo.ts
+++ src/store/modules/todo.ts
@@ -1,26 +1,15 @@
-const ADD_TODO = 'ADD_TODO' as const;
-const TOGGLE_TODO = 'TOGGLE_TODO' as const;
-const REMOVE_TODO = 'REMOVE_TODO' as const;
+import { createAction, ActionType, createReducer } from 'typesafe-actions';
 
-export const addTodo = (text: string) => ({
-  type: ADD_TODO,
-  payload: text,
-});
+const ADD_TODO = 'ADD_TODO';
+const TOGGLE_TODO = 'TOGGLE_TODO';
+const REMOVE_TODO = 'REMOVE_TODO';
 
-export const toggleTodo = (id: number) => ({
-  type: TOGGLE_TODO,
-  payload: id,
-});
+export const addTodo = createAction(ADD_TODO)<string>();
+export const toggleTodo = createAction(TOGGLE_TODO)<number>();
+export const removeTodo = createAction(REMOVE_TODO)<number>();
 
-export const removeTodo = (id: number) => ({
-  type: REMOVE_TODO,
-  payload: id,
-});
-
-type TodosAction =
-  | ReturnType<typeof addTodo>
-  | ReturnType<typeof toggleTodo>
-  | ReturnType<typeof removeTodo>;
+const actions = { addTodo, toggleTodo, removeTodo };
+type TodoAction = ActionType<typeof actions>;
 
 export interface Todo {
   id: number;
@@ -36,26 +25,22 @@ const initialState: TodoState = [
   { id: 3, text: '투두리스트 만들기', done: false },
 ];
 
-function todos(state: TodoState = initialState, action: TodosAction) {
-  switch (action.type) {
-    case ADD_TODO:
-      return state.concat({
-        id: Math.max(...state.map(todo => todo.id)) + 1,
-        text: action.payload,
-        done: false,
-      });
-    case TOGGLE_TODO:
-      return state.map(todo => {
-        if (todo.id === action.payload) {
-          return { ...todo, done: !todo.done };
-        }
-        return todo;
-      });
-    case REMOVE_TODO:
-      return state.filter(todo => todo.id !== action.payload);
-    default:
-      return state;
-  }
-}
+const todos = createReducer<TodoState, TodoAction>(initialState, {
+  [ADD_TODO]: (state, action) =>
+    state.concat({
+      id: Math.max(...state.map(todo => todo.id)) + 1,
+      text: action.payload,
+      done: false,
+    }),
+  [TOGGLE_TODO]: (state, action) =>
+    state.map(todo => {
+      if (todo.id === action.payload) {
+        return { ...todo, done: !todo.done };
+      }
+      return todo;
+    }),
+  [REMOVE_TODO]: (state, action) =>
+    state.filter(todo => todo.id !== action.payload),
+});
 
 export default todos;

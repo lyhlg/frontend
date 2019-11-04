91d25b6a38e315c232ec9f3eb3b39a198c76faf7
Darren Lee <darren.lee@nexybit.com>
2019-11-04 16:44:24 +0900

    console.log 삭제

--- src/api/sample.ts
+++ src/api/sample.ts
@@ -3,23 +3,11 @@ import axios from 'axios';
 export const fetchSample = () =>
   axios
     .get('https://jsonplaceholder.typicode.com/todos/1')
-    .then(resp => {
-      console.log('resp', resp);
-      return resp;
-    })
-    .catch(err => {
-      console.log(err);
-      return err;
-    });
+    .then(resp => resp)
+    .catch(err => err);
 
 export const fetchSample2 = () =>
   axios
-    .get('https://jsonplaceholder.typicode.com/todos/1')
-    .then(resp => {
-      console.log('resp', resp);
-      return resp;
-    })
-    .catch(err => {
-      console.log(err);
-      return err;
-    });
+    .get('https://jsonplaceholder.typicode.com/todos/2')
+    .then(resp => resp)
+    .catch(err => err);
